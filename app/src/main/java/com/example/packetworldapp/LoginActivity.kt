e1e658c6ccb69ef3f7ffd5b183284252d043ef20
ChJulian13 <christianjulianjimenez13@outlook.com>
2026-01-06 08:09:41 -0600

    chore: cambios en objeto conductor

--- app/src/main/java/com/example/packetworldapp/LoginActivity.kt
+++ app/src/main/java/com/example/packetworldapp/LoginActivity.kt
@@ -2,88 +2,68 @@ package com.example.packetworldapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.packetworldapp.databinding.ActivityLoginBinding
+import com.example.packetworldapp.dominio.InicioSesionImp
 import com.example.packetworldapp.dto.RSAutenticacionConductor
-import com.example.packetworldapp.util.Constantes
 import com.google.gson.Gson
-import com.koushikdutta.ion.Ion
-import kotlin.jvm.java
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityLoginBinding
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
         enableEdgeToEdge()
 
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         binding.btnIngresar.setOnClickListener {
-            verificarCredenciales()
-        }
+            val noPersonal = binding.etNoPersonal.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
 
+            if (noPersonal.isEmpty() || password.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Ingrese número de personal y contraseña",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                verificarCredenciales(noPersonal, password)
+            }
+        }
     }
 
-    fun verificarCredenciales() {
-        if (sonCamposValidos()) {
-            consumirAPI(binding.etNoPersonal.text.toString(), binding.etPassword.text.toString())
-        }
-    }
+    private fun verificarCredenciales(noPersonal: String, password: String) {
+        InicioSesionImp.verificarCredenciales(
+            context = this,
+            noPersonal = noPersonal,
+            password = password
+        ) { respuesta: RSAutenticacionConductor ->
 
-    fun sonCamposValidos() : Boolean {
-        var valido = true
-        if (binding.etNoPersonal.text.toString().isEmpty()) {
-            binding.tilNoPersonal.error = "Número de personal obligatorio"
-            valido = false
-        }
-
-        if (binding.etPassword.text.toString().isEmpty()) {
-            binding.tilPassword.error = "Contraseña obligatoria"
-            valido = false
-        }
-
-        return valido
-    }
-
-    fun consumirAPI(noPersonal : String, password : String) {
-        Ion.getDefault(this@LoginActivity).conscryptMiddleware.enable(true)
-        Ion.with(this@LoginActivity)
-            .load("POST", "${Constantes().URL_API}autenticacion/conductor")
-            .setBodyParameter("noPersonal", noPersonal)
-            .setBodyParameter("password", password)
-            .asString()
-            .setCallback {e, result ->
-                if (e == null) {
-                    serializarRespuesta(result)
+            runOnUiThread {
+                if (!respuesta.error) {
+                    Toast.makeText(
+                        this,
+                        "Bienvenido ${respuesta.conductor?.nombre}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    irPantallaInicio(Gson().toJson(respuesta.conductor))
                 } else {
-                    Toast.makeText(this@LoginActivity, "Error de conexión: ${e.message}", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this,
+                        respuesta.mensaje,
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
-    }
-
-    fun serializarRespuesta(json : String) {
-        Log.e("WS", json)
-        try {
-            val gson : Gson = Gson()
-            val respuestaLogin = gson.fromJson(json, RSAutenticacionConductor::class.java)
-            if (!respuestaLogin.error) {
-                Toast.makeText(this@LoginActivity, "Bienvenido(a) ${respuestaLogin.conductor!!.nombre}", Toast.LENGTH_SHORT).show()
-                irPantallaInicio(json)
-            } else {
-                Toast.makeText(this@LoginActivity, "Error: ${respuestaLogin.mensaje}", Toast.LENGTH_SHORT).show()
-            }
-        } catch (e : Exception) {
-            Toast.makeText(this@LoginActivity, "Lo sentimos, hubo un error en la solicitud: ${e.message}", Toast.LENGTH_SHORT).show()
         }
     }
 
-    fun irPantallaInicio(json : String) {
+    private fun irPantallaInicio(json: String) {
         val intent = Intent(this@LoginActivity, MainActivity::class.java)
         intent.putExtra("conductor", json)
         startActivity(intent)
--- app/src/main/java/com/example/packetworldapp/dto/RSAutenticacionConductor.kt
+++ app/src/main/java/com/example/packetworldapp/dto/RSAutenticacionConductor.kt
@@ -1,9 +1,13 @@
 package com.example.packetworldapp.dto
 
 import com.example.packetworldapp.poko.Conductor
+import com.google.gson.annotations.SerializedName
 
 data class RSAutenticacionConductor(
-    val error : Boolean,
-    val mensaje : String,
-    val conductor : Conductor?
+    var error: Boolean = false,
+
+    var mensaje: String = "",
+
+    @SerializedName("colaborador")
+    var conductor: Conductor? = null
 )
