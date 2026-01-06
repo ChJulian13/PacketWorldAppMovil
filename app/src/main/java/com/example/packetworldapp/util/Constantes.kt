64af1bc096a229ad0b64d14c46a1727d8420f3f9
ChJulian13 <christianjulianjimenez13@outlook.com>
2026-01-05 19:33:58 -0600

    Chore: implementar constantes necesarias para el proyecto

--- app/src/main/java/com/example/packetworldapp/util/Constantes.kt
+++ app/src/main/java/com/example/packetworldapp/util/Constantes.kt
@@ -1,5 +1,11 @@
 package com.example.packetworldapp.util
 
-class Constantes {
-    val URL_API = "http://192.168.56.1:8080/PacketWorldAPI/webresources/"
+object Constantes {
+    const val URL_API = "http://192.168.56.1:8080/PacketWorldAPI/webresources/"
+    const val ERROR_MALFORMED_URL: Int = 1001
+    const val ERROR_PETICION: Int = 1002
+    const val MSJ_ERROR_URL: String =
+        "Lo sentimos su solicitud no se puede realizar, por favor intentelo más tarde."
+    const val MSJ_ERROR_PETICION: String =
+        "Lo sentimos tenemos problemas de conexión en este momento, por favor intentelo más tarde."
 }
